b6f0d6d2f5076ce927f61d975a0afc83cfab1f66
KMikaze1202 <yannick.fayet1995@gmail.com>
2014-11-20 14:50:06 +0100

    srpint 1: Affichage du rapport

--- ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.Designer.cs
+++ ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.Designer.cs
@@ -28,47 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblRapport = new System.Windows.Forms.Label();
+            this.dtpDateVisite = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // lblRapport
             // 
-            this.button1.Location = new System.Drawing.Point(175, 328);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lblRapport.AutoSize = true;
+            this.lblRapport.Location = new System.Drawing.Point(108, 131);
+            this.lblRapport.Name = "lblRapport";
+            this.lblRapport.Size = new System.Drawing.Size(35, 13);
+            this.lblRapport.TabIndex = 0;
+            this.lblRapport.Text = "label1";
             // 
-            // dataGridView1
+            // dtpDateVisite
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(403, 215);
-            this.dataGridView1.TabIndex = 1;
+            this.dtpDateVisite.Location = new System.Drawing.Point(37, 81);
+            this.dtpDateVisite.Name = "dtpDateVisite";
+            this.dtpDateVisite.Size = new System.Drawing.Size(200, 20);
+            this.dtpDateVisite.TabIndex = 1;
+            this.dtpDateVisite.Value = new System.DateTime(2014, 11, 21, 0, 0, 0, 0);
+            this.dtpDateVisite.ValueChanged += new System.EventHandler(this.dtpDateVisite_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 411);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtpDateVisite);
+            this.Controls.Add(this.lblRapport);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblRapport;
+        private System.Windows.Forms.DateTimePicker dtpDateVisite;
     }
 }
 
--- ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.cs
+++ ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 
 namespace ProjetCabanesGroupeA
@@ -26,6 +27,17 @@ namespace ProjetCabanesGroupeA
         private void Form1_Load(object sender, EventArgs e)
         {
             
+        }
+
+
+        private void dtpDateVisite_ValueChanged(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.CommandText = "SELECT * FROM t_visites WHERE id_visite =" + list[0] + "AND date_visite=" + Convert.ToString(dtpDateVisite.Value);
+            cmd.CommandType = CommandType.Text;
+
+            lblRapport.Text = cmd.CommandText;
         }
     }
 }
