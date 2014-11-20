b848c55a2d89702e38420c14cf867e18b8fc5c8b
SolidJordi <jordan.dacuna.rodriguez@gmail.com>
2014-11-20 15:11:39 +0100

    Sprint 1 : Modification du nom de l'application. Ajout ListeBox Cabane et un bouton affiche liste cabane. L'application se compile bien.

--- ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.Designer.cs
+++ ProjetCabanesGroupeA/ProjetCabanesGroupeA/Form1.Designer.cs
@@ -30,9 +30,12 @@
         {
             this.lblRapport = new System.Windows.Forms.Label();
             this.dtpDateVisite = new System.Windows.Forms.DateTimePicker();
+            this.BtnShowListCabin = new System.Windows.Forms.Button();
+            this.LbxCabin = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // lblRapport
+            // BtnShowListCabin
             // 
             this.lblRapport.AutoSize = true;
             this.lblRapport.Location = new System.Drawing.Point(108, 131);
@@ -40,14 +43,26 @@
             this.lblRapport.Size = new System.Drawing.Size(35, 13);
             this.lblRapport.TabIndex = 0;
             this.lblRapport.Text = "label1";
+            this.BtnShowListCabin.Location = new System.Drawing.Point(12, 282);
+            this.BtnShowListCabin.Name = "BtnShowListCabin";
+            this.BtnShowListCabin.Size = new System.Drawing.Size(137, 23);
+            this.BtnShowListCabin.TabIndex = 0;
+            this.BtnShowListCabin.Text = "Affiche Liste Cabane";
+            this.BtnShowListCabin.UseVisualStyleBackColor = true;
             // 
             // dtpDateVisite
+            // LbxCabin
             // 
             this.dtpDateVisite.Location = new System.Drawing.Point(37, 81);
             this.dtpDateVisite.Name = "dtpDateVisite";
             this.dtpDateVisite.Size = new System.Drawing.Size(200, 20);
             this.dtpDateVisite.TabIndex = 1;
             this.dtpDateVisite.Value = new System.DateTime(2014, 11, 21, 0, 0, 0, 0);
+            this.LbxCabin.FormattingEnabled = true;
+            this.LbxCabin.Location = new System.Drawing.Point(12, 12);
+            this.LbxCabin.Name = "LbxCabin";
+            this.LbxCabin.Size = new System.Drawing.Size(120, 251);
+            this.LbxCabin.TabIndex = 1;
             this.dtpDateVisite.ValueChanged += new System.EventHandler(this.dtpDateVisite_ValueChanged);
             // 
             // Form1
@@ -57,8 +72,10 @@
             this.ClientSize = new System.Drawing.Size(427, 411);
             this.Controls.Add(this.dtpDateVisite);
             this.Controls.Add(this.lblRapport);
+            this.Controls.Add(this.LbxCabin);
+            this.Controls.Add(this.BtnShowListCabin);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cabanes";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,6 +86,9 @@
 
         private System.Windows.Forms.Label lblRapport;
         private System.Windows.Forms.DateTimePicker dtpDateVisite;
+        private System.Windows.Forms.Button BtnShowListCabin;
+        private System.Windows.Forms.ListBox LbxCabin;
+
     }
 }
 
